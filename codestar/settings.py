7fff227b183bbe3f54054fd5440bf2194829a6a1
Diana Fedyachkina <dfedyachkinaa@icloud.com>
2025-01-04 23:39:37 +0000

    Set up comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['8000-dfedyachkina-djangoblog-b7n88e1aow5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -50,11 +51,18 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
